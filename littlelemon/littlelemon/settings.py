9bafcd5a6b5c5f13bd727a890c54440b559940a3
RequiemOV <klookstar8@gmail.com>
2023-03-17 22:05:32 +0200

    Added_secure_testing
    Securing and testing are added

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'rest_framework',
+    'djoser',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER={"USER_ID_FIELD":"username"}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+     'rest_framework.authentication.SessionAuthentication',
+     'rest_framework.authentication.TokenAuthentication',
+     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+    'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+}
---
+++ littlelemon/littlelemon/tests/test-models.py
@@ -0,0 +1,6 @@
+from django.test import testcases
+from restaurant.models import *
+class MenuTest(testcases):
+    def test_get_item(self):
+        item = Menu.objects.create(title="IceCream", price=80, inventory=100)
+        self.assertEqual(item, "IceCream : 80")
